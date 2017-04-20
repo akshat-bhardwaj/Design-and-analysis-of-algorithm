7f12b1eb4566927110c1501c9c9af0d1f737c9a5
Akshat Bhardwaj <akshatbhardwaj227@gmail.com>
2017-04-20 19:27:43 +0530

    Update GrahamScan.cpp

--- Assignment 4/GrahamScan.cpp
+++ Assignment 4/GrahamScan.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-class mypoint{
+class mp{
 	public:
 	    int x, y;
-	    bool operator < (mypoint b) {
+	    bool operator < (mp b) {
 	        if (y != b.y)
 	            return y < b.y;
 	        return x < b.x;
 	    }
 };
-mypoint pivot;
-int Orient(mypoint a, mypoint b, mypoint c) {
+mp pivot;
+int Orient(mp a, mp b, mp c) {
     int val = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
       if (val == 0)
         return 0;
     return (val > 0) ? -1 : 1;
 }
-int Dist_Sq(mypoint a, mypoint b)  {
+int Dist_Sq(mp a, mp b)  {
     int dx = a.x - b.x, dy = a.y - b.y;
     return dx * dx + dy * dy;
 }
-bool polar_order(mypoint a, mypoint b)  {
+bool polar_order(mp a, mp b)  {
     int order = Orient(pivot, a, b);
     if (order == 0)
         return Dist_Sq(pivot, a) < Dist_Sq(pivot, b);
